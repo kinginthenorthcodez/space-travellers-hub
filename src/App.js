691dcc0b9e6dabc93f2af49a798c35c9649e5da7
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-21 10:33:45 +0100

    Import Header component within App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import './App.css';
-
-function App() {
-  return <h2>Space Travellers!</h2>;
-=======
-import "./App.css";
-import Nav from "./components/Nav";
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <Nav />
+      <Header />
     </>
   );
->>>>>>> cf42e1e91f4a2ab2d600f7935a0c0385bd9bf88b
 }
 
 export default App;
