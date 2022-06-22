33053f0573ba5e350bd406d44e255702c7238ddf
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-22 01:09:34 +0100

    Create route for Missions page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Missions from './components/pages/Missions';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="missions" element={<Missions />} />
+      </Routes>
     </>
   );
 }
