d50e3f1a885fee0c2a31cf400c4b27bf18d5a4c8
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-22 12:06:36 +0100

    Enclose table displaying missions in a container and assign class

--- src/components/pages/Missions.js
+++ src/components/pages/Missions.js
@@ -1,5 +1,6 @@
 // import React from 'react';
 import Mission from '../Mission';
+import '../css/Missions.css';
 
 const Missions = () => {
   const missionsObject = [
@@ -24,26 +25,29 @@ const Missions = () => {
   ];
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {missionsObject.map((mission) => (
-          <Mission
-            key={mission.id}
-            id={mission.id}
-            name={mission.name}
-            description={mission.description}
-            member={mission.member}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className="missionsContainer">
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missionsObject.map((mission) => (
+            <Mission
+              key={mission.id}
+              id={mission.id}
+              name={mission.name}
+              description={mission.description}
+              member={mission.member}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
