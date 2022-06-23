8bcbbccd42acb77493495d566aa32692eae4716e
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-23 11:58:02 +0100

    Create missions actions

---
+++ src/redux/Missions/Missions.js
@@ -0,0 +1,6 @@
+const apiURL = () => 'https://api.spacexdata.com/v3/missions';
+
+export const Actions = {
+  LOAD: 'space-travellers-hub/missions/LOAD',
+  TOGGLE_JOIN: 'space-travellers-hub/missions/TOGGLE_JOIN',
+};
