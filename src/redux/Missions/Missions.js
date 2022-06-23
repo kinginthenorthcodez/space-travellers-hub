b6047f3e1d625c997de592ddd1bf590170ff8648
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-23 12:01:22 +0100

    Write missions action creators

--- src/redux/Missions/Missions.js
+++ src/redux/Missions/Missions.js
@@ -6,3 +6,26 @@ export const Actions = {
 };
 
 const stateInit = [];
+
+export const loadMissions = () => async (dispatch) => {
+  const response = await fetch(apiURL());
+  const data = await response.json();
+
+  const payLoad = data.map((mission) => ({
+    id: mission.mission_id,
+    name: mission.mission_name,
+    description: mission.description,
+    member: false,
+  }));
+
+  if (response.ok) {
+    dispatch({
+      type: Actions.LOAD,
+      payLoad,
+    });
+  }
+};
+
+export const toggleJoinMission = (missionId) => (
+  { type: Actions.TOGGLE_JOIN, payLoad: { id: missionId } }
+);
