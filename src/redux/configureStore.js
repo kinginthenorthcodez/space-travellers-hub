f8f784d38a31ce3c9335281ec42f960c019e4662
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-23 12:03:31 +0100

    Combine missions reducer with other reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import dragonsReducer, { fetchDataAPI } from './dragons';
+import missions from './Missions/Missions';
 
 const allReducers = combineReducers({
   dragonsReducer,
+  missions,
 });
 const store = createStore(allReducers, applyMiddleware(logger, thunk));
 store.dispatch(fetchDataAPI());
