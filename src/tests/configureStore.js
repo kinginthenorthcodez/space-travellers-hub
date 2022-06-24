0f74059351be8c77f4125c79c7d8d6a2265f65e1
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-06-24 06:03:19 -0300

    Fix linter errors

--- src/tests/configureStore.js
+++ src/tests/configureStore.js
@@ -1,26 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import showRocket from '../redux/rockets/reducer';
-import dragonsReducer from "../redux/dragons/index";
+import dragonsReducer from '../redux/dragons/index';
 
 const setupStore = (reserved) => {
   const preloadedState = {
     rockets: [
       {
         id: 1,
-        name: "Falcon 1",
+        name: 'Falcon 1',
         description:
-          "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.",
-        img: "https://imgur.com/DaCfMsj.jpg",
+          'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
+        img: 'https://imgur.com/DaCfMsj.jpg',
         reserved,
       },
     ],
     dragons: [
       {
         id: 1,
-        name: "Falcon 1",
+        name: 'Falcon 1',
         description:
-          "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.",
-        img: "https://imgur.com/DaCfMsj.jpg",
+          'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
+        img: 'https://imgur.com/DaCfMsj.jpg',
         reserved,
       },
     ],
--- src/tests/rockets.test.js
+++ src/tests/rockets.test.js
@@ -1,15 +1,15 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import setupStore from "./configureStore";
-import RocketExihbit from "../components/rockets/Rockets";
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import setupStore from './configureStore';
+import RocketExihbit from '../components/rockets/Rockets';
 
-describe("Test the Rockets.test components", () => {
-  it("renders the Rockets page correctly", () => {
+describe('Test the Rockets.test components', () => {
+  it('renders the Rockets page correctly', () => {
     const store = setupStore(false);
     const page = render(
       <Provider store={store}>
         <RocketExihbit />
-      </Provider>
+      </Provider>,
     );
     expect(page).toMatchSnapshot();
   });
@@ -19,13 +19,13 @@ describe("Test the Rockets.test components", () => {
     render(
       <Provider store={store}>
         <RocketExihbit />
-      </Provider>
+      </Provider>,
     );
 
-    expect(screen.queryByText("Cancel Reservation")).toBeFalsy();
-    fireEvent.click(screen.queryByText("Reserve Rocket"));
-    expect(screen.queryByText("Cancel Reservation")).toBeTruthy();
-    expect(screen.queryByText("Reserve Rocket")).toBeFalsy();
+    expect(screen.queryByText('Cancel Reservation')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Reserve Rocket'));
+    expect(screen.queryByText('Cancel Reservation')).toBeTruthy();
+    expect(screen.queryByText('Reserve Rocket')).toBeFalsy();
   });
 
   it('shows the button changes from "Cancel Reservation" to "Reserve Rocket" on click event', () => {
@@ -33,38 +33,38 @@ describe("Test the Rockets.test components", () => {
     render(
       <Provider store={store}>
         <RocketExihbit />
-      </Provider>
+      </Provider>,
     );
 
-    expect(screen.queryByText("Reserve Rocket")).toBeFalsy();
-    fireEvent.click(screen.queryByText("Cancel Reservation"));
-    expect(screen.queryByText("Reserve Rocket")).toBeTruthy();
-    expect(screen.queryByText("Cancel Reservation")).toBeFalsy();
+    expect(screen.queryByText('Reserve Rocket')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Cancel Reservation'));
+    expect(screen.queryByText('Reserve Rocket')).toBeTruthy();
+    expect(screen.queryByText('Cancel Reservation')).toBeFalsy();
   });
 
-  it("shows the reserved badge on click event", () => {
+  it('shows the reserved badge on click event', () => {
     const store = setupStore(false);
     render(
       <Provider store={store}>
         <RocketExihbit />
-      </Provider>
+      </Provider>,
     );
 
-    expect(screen.queryByText("Reserved")).toBeFalsy();
-    fireEvent.click(screen.queryByText("Reserve Rocket"));
-    expect(screen.queryByText("Reserved")).toBeTruthy();
+    expect(screen.queryByText('Reserved')).toBeFalsy();
+    fireEvent.click(screen.queryByText('Reserve Rocket'));
+    expect(screen.queryByText('Reserved')).toBeTruthy();
   });
 
-  it("hides the reserved badge on click event", () => {
+  it('hides the reserved badge on click event', () => {
     const store = setupStore(true);
     render(
       <Provider store={store}>
         <RocketExihbit />
-      </Provider>
+      </Provider>,
     );
 
-    expect(screen.queryByText("Reserved")).toBeTruthy();
-    fireEvent.click(screen.queryByText("Cancel Reservation"));
-    expect(screen.queryByText("Reserved")).toBeFalsy();
+    expect(screen.queryByText('Reserved')).toBeTruthy();
+    fireEvent.click(screen.queryByText('Cancel Reservation'));
+    expect(screen.queryByText('Reserved')).toBeFalsy();
   });
 });
